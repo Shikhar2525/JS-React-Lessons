9ede89a5ff5da1eb027ff738d96018af47da74a8
Shikhar Mandloi <shikhar.mandloi@gmail.com>
2024-12-02 14:38:23 +0530

    lesson 1

---
+++ JavaScript/Lesson1.js
@@ -0,0 +1,57 @@
+// let a = 3;
+// let b = 5;
+
+// Ways to display content in js
+// document.getElementById("demo").innerHTML = a + b;
+
+// document.write("Hello javascript");
+
+// window.alert("This is an alert");
+
+// console.log("api key is: sdsds23skdsdkc");
+
+// Variables
+// var  //  -> public variable
+// let  //  -> blocked scoped , value can changed
+// const // -> blocked scoped , value cannot be changed
+
+// {
+//   var a = 2;
+// }
+
+// document.write(a);
+
+// const a = 2;
+// document.write(a);
+// a = 4;
+// document.write(a);
+
+// let a = {};
+
+// document.write(typeof a); // object
+
+// function abc(a, b, c) {
+//   return a + b + c;
+// }
+
+// const abc = (a, b, c) => {
+//   return a + b + c;
+// };
+
+// document.write(abc(2, 3, 4));
+
+// const expense = {
+//   expenseTitle: "abc",
+//   amount: 500,
+//   firstName: "Shikhar",
+//   lastName: "Mandloi",
+//   //   giveFullName: function () {
+//   //     return this.firstName + " " + this.lastName;
+//   //   },
+// };
+
+// // document.write(expense["expenseTitle"]);
+// // document.write(expense.expenseTitle);
+// // document.write(expense.giveFullName());
+
+// document.write(JSON.stringify(expense));
